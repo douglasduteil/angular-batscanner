ff44b15d4e5621f6075d941cea543cd4eda4a519
Douglas Duteil <douglasduteil@gmail.com>
2016-10-09 14:51:46 +0200

    SAVE successfully made dummy flames <3

--- packages/angular-batscanner-devtool/src/devtool/graph/graph_component.js
+++ packages/angular-batscanner-devtool/src/devtool/graph/graph_component.js
@@ -93,7 +93,35 @@ Component({
 
   ngOnChanges (changes) {
     if (changes.state && this._render) {
-      this._render(this.state)
+      const lastest = this.data[this.data.length - 1] || {
+        timestamp: 1000 + (Math.random() * 1000),
+        duration: 1000 + (Math.random() * 1000)
+      }
+      var data = [1, 2, 4, 2, 1]
+      var maxDeph = data.reduce((memo, val) => {
+        if (memo.indexOf(val) <= -1) { memo.push(val) }
+        return memo
+      }, [])
+      console.log(maxDeph)
+      var depth = d3.scaleLinear()
+        .domain(d3.extent(data))
+        .range([0, maxDeph.length - 1, 1])
+        .interpolate(d3.interpolateRound)
+        .clamp(true)
+
+      const statee = data.reduce(function (memo, val) {
+        const lastValue = (memo[memo.length - 1] || lastest)
+        memo.push({
+          timestamp: lastValue.timestamp + lastValue.duration,
+          duration: 1000 + (Math.random() * 1000),
+          depth: depth(val)
+        })
+        return memo
+      }, [])
+
+      statee.forEach((e) => { this.data.push(e) })
+
+      this._render()
     }
   },
 
@@ -107,47 +135,128 @@ Component({
   //
 
   _initializeGraph (svgElement, tooltipElement) {
+    console.log('_initializeGraph', svgElement, tooltipElement)
     const margin = {top: 90, right: 0, bottom: 30, left: 0}
 
-    const svg = d3.select(svgElement)
+    const svg = this.svg = d3.select(svgElement)
 
     const svgWidth = svgElement.clientWidth
     const svgHeight = svgElement.clientHeight
 
     const width = svgWidth - margin.left - margin.right
     const height = svgHeight - margin.top - margin.bottom
+    this.color = d3.scaleOrdinal(d3.schemeCategory10)
 
-    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    //
 
-    this.g = svg.append('g').attr('transform', 'translate(32,' + (height / 2) + ')')
+    const x = this.x = d3.scaleLinear().domain([0, 1000]).range([0, width])
+    const y = this.y = d3.scaleLinear().domain([0, 10]).range([0, height])
+    const xAxis = this.xAxis = d3.axisBottom(x)
+    const yAxis = this.yAxis = d3.axisLeft(y)
 
-    this.flamegraph = flameGraph()
-      .width(width)
-      .height(height)
+    svg.append('g')
+    .attr('class', 'axis axis--x')
+    .call(xAxis)
 
-    this.flamechart = d3.call(this.flamegraph);
+    svg.append('g')
+    .attr('class', 'axis axis--y')
+    .call(yAxis)
+
+    this.flamechart = svg.append('g')
+      .attr('class', 'flamechart')
+      .attr('transform', 'translate(0, 50)')
+
+    this.rectangles = svg.append('g')
+      .attr('class', 'rectangles')
+      .attr('transform', 'translate(0, 50)')
   },
 
   _render () {
-    if (!this.g) {
+    const data = this.data
+    console.log('_render', data.slice(0))
+    if (!this.rectangles) {
+      return
+    }
+    const {color, flamechart, svg, x, xAxis, y, yAxis} = this
+
+    const minmaxdomain = d3.extent([]
+      .concat(d3.extent(data, (d) => d.timestamp - 1000))
+      .concat(d3.extent(data, (d) => d.timestamp + d.duration + 1000))
+    )
+    x.domain(minmaxdomain)
+    y.domain([0, Math.max(data.length, 10)])
+    svg.select('.axis--x').call(xAxis)
+    svg.select('.axis--y').call(yAxis)
+
+    const minX = x.domain()[0]
+    var selection = this.rectangles
+      .selectAll('.bar')
+      .data(data)
+
+    selection.enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .merge(selection)
+       .attr('transform', (d, i) => `translate(${x(d.timestamp)}, ${y(d.depth)})`)
+      .attr('fill', (d, i) => d3.rgb(color(i)).brighter(1.5))
+      .attr('height', (d) => y(1))
+      .attr('width', (d) => x(minX + d.duration))
+  },
+
+  _renderr () {
+    const data = this.data
+    console.log('_render', data)
+    if (!this.flamechart) {
       return
     }
 
-    var t = d3.transition()
-        .duration(750)
+    const {color, flamechart, svg, x, xAxis} = this
+    const itemHeight = 50
+    const textMargin = 5
 
-    const data = d3.shuffle( [
-                  [10, 10], [100, 10], [100, 100], [10, 100]
-                  ])
+    const startTime = d3.min(data, (d) => d.timestamp)
+    const endTime = d3.max(data, (d) => d.timestamp)
+    x.domain([startTime - 2000, endTime + 2000])
+    svg.select('.axis').call(xAxis)
 
-    const path = this.g.selectAll('path').data([data])
-    path.attr('d', (d) => this.line(d) + 'Z')
-        .style('stroke-width', 1)
-        .style('stroke', 'steelblue');
-    path.enter().append('svg:path').attr('d', (d) => this.line(d) + 'Z')
-        .style('stroke-width', 1)
-        .style('stroke', 'steelblue');
-    path.exit().remove()
+    //
+
+    const newSections = flamechart.selectAll('g.section')
+      .data(data)
+
+    newSections.exit().remove()
+
+    newSections
+      .enter()
+      .append('rect')
+      .merge(newSections)
+      .attr('width', (d) => x(x.domain()[0] + d.duration))
+      .attr('height', itemHeight)
+      .attr('fill', (d, i) => d3.rgb(color(d.type)).brighter(1.5))
+      .style('stroke', '#fff')
+      .style('stroke-width', 1)
+
+    // const block = newSections
+    // .merge(newSections.selectAll('.block'))
+    //   .attr('transform', (d, i) => `translate(${x(d.start)}, ${d.id * itemHeight})`)
+
+    // block
+    //   .append('rect')
+    //   .attr('width', (d) => x(x.domain()[0] + d.duration))
+    //   .attr('height', itemHeight)
+    //   .attr('fill', (d, i) => d3.rgb(color(d.type)).brighter(1.5))
+    //   .style('stroke', '#fff')
+    //   .style('stroke-width', 1)
+
+    // newSections
+    //   .append('foreignObject')
+    //   .append('xhtml:div')
+    //   .attr('class', 'label')
+    //   .style('display', (d) => (x(x.domain()[0] + d.duration) < 100) ? 'none' : 'block')
+    //   .attr('x', (d) => textMargin)
+    //   .attr('y', (d) => itemHeight * (2.25 / 3))
+    //   .attr('fill', '#000')
+    //   .text((d) => `${d.type} @ ${d.name}`)
   }
   //
 })
