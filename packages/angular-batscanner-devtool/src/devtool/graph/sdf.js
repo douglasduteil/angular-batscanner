7a75efad3fdd0d5d1deeacf9d9946db3d497c29e
Douglas Duteil <douglasduteil@gmail.com>
2016-10-20 21:27:22 +0200

    feat(devtool): SAVE not best but correct axis polylinear view

--- packages/angular-batscanner-devtool/src/devtool/graph/sdf.js
+++ packages/angular-batscanner-devtool/src/devtool/graph/sdf.js
@@ -5,6 +5,7 @@ import * as d3 from 'd3'
 //
 
 const dis = 20
+const AXIS_SUBTICKS = 10
 
 export function polylinearRangeFromDomains (options) {
   const [min, max] = options.range
@@ -39,10 +40,25 @@ export function axisTicks (options) {
         .domain(domain)
         .range(x.range().slice(i * 2, (i * 2) + 2))
     )
-  const rDistance = distance(d3.extent(x.domain()))
-  const ticks = seriesScales.reduce((memo, scale) => {
-    const coef = distance(scale.domain()) / rDistance
-    return memo.concat(scale.ticks(10 * coef))
+
+  const {distances, totalDistance} = seriesScales.reduce(
+    (memo, scale) => {
+      const d = distance(scale.domain())
+      memo.distances.push(d)
+      memo.totalDistance += d
+      return memo
+    },
+    {
+      distances: [],
+      totalDistance: 0
+    }
+  )
+
+  const ticks = seriesScales.reduce((memo, scale, i) => {
+    const dFract = distances[i] / totalDistance
+    let ticksCount = Math.floor(AXIS_SUBTICKS * dFract)
+    ticksCount = ticksCount || (i % 2 ? 1 : 0)
+    return memo.concat(scale.ticks(ticksCount))
   }, [])
 
   return ticks
